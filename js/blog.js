b164a553c3732a4a05fc9c3fbf78cc9758c15315
EmmanuelAyo2233 <emmanuelayobami61@gmail.comgit config --global user.name EmmanuelAyo2233>
2024-10-25 17:25:01 +0100

    git add .

---
+++ js/blog.js
@@ -0,0 +1,58 @@
+ScrollReveal().reveal('.fade-in', {
+    duration: 1000,
+    distance: '50px',
+    origin: 'bottom',
+    easing: 'ease-in-out',
+    beforeReveal: function (el) {
+        el.classList.add('show');
+    }
+});
+
+ScrollReveal().reveal('.fade-in-delay', {
+    duration: 1000,
+    distance: '50px',
+    origin: 'left',
+    easing: 'ease-in-out',
+    easing: 'ease-in-out',
+    beforeReveal: function (el) {
+        el.classList.add('show');
+    }
+});
+
+ScrollReveal().reveal('.top', {
+    duration: 1000,
+    distance: '50px',
+    origin: 'top',
+    easing: 'ease-in-out',
+    easing: 'ease-in-out',
+    beforeReveal: function (el) {
+        el.classList.add('show');
+    }
+});
+
+ScrollReveal().reveal('.fade-in-delay-2', {
+    duration: 1000,
+    distance: '50px',
+    origin: 'right',
+    easing: 'ease-in-out',
+    easing: 'ease-in-out',
+    beforeReveal: function (el) {
+        el.classList.add('show');
+    }
+});
+
+
+window.addEventListener('scroll', function() {
+    let scrollButton = document.getElementById('scrollUpButton');
+    if (window.scrollY > 100) {
+        scrollButton.style.display = 'block';
+    } else {
+        scrollButton.style.display = 'none';
+    }
+});
+
+
+document.getElementById('scrollUpButton').addEventListener('click', function(e) {
+    e.preventDefault();
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+});
